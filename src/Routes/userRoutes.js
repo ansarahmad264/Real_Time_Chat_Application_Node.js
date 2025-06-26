67b628d497662502f2b24a668ed1b99522bd6c48
Ansar Ahmad <ahmadansar264@gmail.com>
2025-06-26 15:42:50 +0500

    Add user Login functionality with Runtime Token generation

--- src/Routes/userRoutes.js
+++ src/Routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { userSignup } from "../Controllers/userController.js"
+import { userLogin, userSignup } from "../Controllers/userController.js"
 import { upload } from "../Middlewares/multer.js"
 
 const router = express.Router()
 
 router.route("/signup").post(upload.single("profilePic"), userSignup)
+router.route("/login").post(userLogin)
 
 
 export default router
