17af31bfdb30f8d180471bb9ac3657951c3decb3
Ansar Ahmad <ahmadansar264@gmail.com>
2025-06-30 15:31:38 +0500

    Add Passport.js configuration

---
+++ src/config/passport.js
@@ -0,0 +1,40 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import dotenv from 'dotenv';
+import User from '../models/user.model.js';
+
+dotenv.config();
+
+passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          const { sub: googleId, email, name: fullName, picture } = profile._json;
+          const username = email.split('@')[0];
+  
+          let user = await User.findOne({ email });
+  
+          if (!user) {
+            user = await User.create({
+              fullName,
+              email,
+              googleId,
+              username,
+              profilePic: picture,
+              authProvider: "google"
+            });
+          }
+  
+          return done(null, user);
+        } catch (error) {
+          return done(error, false);
+        }
+      }
+    )
+  );
+  
