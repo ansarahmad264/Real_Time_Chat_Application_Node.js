30ab7cd3ac094bf4eec0c26e30e79c7fcaaef14b
Ansar Ahmad <ahmadansar264@gmail.com>
2025-07-03 14:55:24 +0500

    Code Upgrade in Get emssages endpoint to reshape Response

--- src/Controllers/messageController.js
+++ src/Controllers/messageController.js
@@ -3,10 +3,10 @@ import Message from "../Models/message.model.js"
 import { asyncHandler } from "../Utils/asyncHandler.js";
 import { getRecieverSocketId, io } from "../socket/socket.js"
 
-const sendMessage = asyncHandler(async(req,res) =>{
+const sendMessage = asyncHandler(async (req, res) => {
     try {
-        const {message} = req.body
-        const{recieverId } = req.params
+        const { message } = req.body
+        const { recieverId } = req.params
         const senderId = req.user._id
 
         var conversation = await Conversation.findOne({
@@ -15,7 +15,7 @@ const sendMessage = asyncHandler(async(req,res) =>{
             }
         })
 
-        if(!conversation){
+        if (!conversation) {
             conversation = await Conversation.create({
                 participants: [senderId, recieverId]
             })
@@ -32,8 +32,8 @@ const sendMessage = asyncHandler(async(req,res) =>{
         await Promise.all([conversation.save(), newMessage.save()])
 
         const recieverSocketId = getRecieverSocketId(recieverId)
-        
-        if(recieverSocketId){
+
+        if (recieverSocketId) {
             io.to(recieverSocketId).emit("newMessage", newMessage)
         }
         /* else{
@@ -50,34 +50,42 @@ const sendMessage = asyncHandler(async(req,res) =>{
         } */
 
         res.status(201).json(newMessage)
-        
+
     } catch (error) {
-        console.log("Error in SendMessage Controler",error.message)
-        return res.status(500).json({error: "internal Server Error"})
+        console.log("Error in SendMessage Controler", error.message)
+        return res.status(500).json({ error: "internal Server Error" })
     }
 })
 
-const getMessages = asyncHandler(async(req,res) => {
+const getMessages = asyncHandler(async (req, res) => {
     try {
-        const{userToChatId} = req.params
+        const { userToChatId } = req.params
         const senderId = req.user._id
 
         const conversation = await Conversation.findOne({
-            participants: {$all: [senderId, userToChatId]}
+            participants: { $all: [senderId, userToChatId] }
         })
 
-        if(!conversation){
+        if (!conversation) {
             return res.status(404).json({ message: "No conversation found" });
         }
 
-        const messages = await Message.find({ conversationId: conversation._id })
-      .sort({ createdAt: 1 }); // Optional: oldest first
+        const messagesRaw = await Message.find({ conversationId: conversation._id })
+            .sort({ createdAt: 1 }) // oldest first
+            .populate("senderId", "fullName username")
+            .populate("recieverId", "fullName username");
 
+        const messages = messagesRaw.map(msg => ({
+            _id: msg._id,
+            Sender: `${msg.senderId.fullName} (${msg.senderId.username})`,
+            Receiver: `${msg.recieverId.fullName} (${msg.recieverId.username})`,
+            Message: msg.message
+        }));
         res.status(200).json(messages)
-        
+
     } catch (error) {
-        console.log("Error in getMessage Controller",error.message)
-        return res.status(500).json({error: "internal Server Error"})
+        console.log("Error in getMessage Controller", error.message)
+        return res.status(500).json({ error: "internal Server Error" })
     }
 })
 
