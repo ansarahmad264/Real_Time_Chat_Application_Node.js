74bacb88f3986510ec6edd15e13fd6aeee07a41a
Ansar Ahmad <ahmadansar264@gmail.com>
2025-07-09 12:58:44 +0500

    Add emails utils functions for reset request and success

---
+++ src/Utils/emails.js
@@ -0,0 +1,46 @@
+import {
+	PASSWORD_RESET_REQUEST_TEMPLATE,
+	PASSWORD_RESET_SUCCESS_TEMPLATE,
+} from "./emailTemplates.js";
+import { mailtrapClient, sender } from "../config/mailTrap.config.js"
+import {asyncHandler} from "./asyncHandler.js"
+
+
+
+export const sendPasswordResetEmail = asyncHandler(async (email, resetURL) => {
+	const recipient = [{ email }];
+
+	try {
+		const response = await mailtrapClient.send({
+			from: sender,
+			to: recipient,
+			subject: "Reset your password",
+			html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+			category: "Password Reset",
+		});
+	} catch (error) {
+		console.error(`Error sending password reset email`, error);
+
+		throw new Error(`Error sending password reset email: ${error}`);
+	}
+});
+
+export const sendResetSuccessEmail = asyncHandler(async (email) => {
+	const recipient = [{ email }];
+
+	try {
+		const response = await mailtrapClient.send({
+			from: sender,
+			to: recipient,
+			subject: "Password Reset Successful",
+			html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+			category: "Password Reset",
+		});
+
+		console.log("Password reset email sent successfully", response);
+	} catch (error) {
+		console.error(`Error sending password reset success email`, error);
+
+		throw new Error(`Error sending password reset success email: ${error}`);
+	}
+});
