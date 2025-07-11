923e78c45e9ebac53f9224d025fd0346e66a9dfe
Ansar Ahmad <ahmadansar264@gmail.com>
2025-07-11 15:23:01 +0500

    add middle ware in send message route

--- src/Routes/messageRoutes.js
+++ src/Routes/messageRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { getMessages, sendMessage } from "../Controllers/messageController.js"
 import { verifyJWT } from "../Middlewares/Auth.js"
+import { dmProtect } from "../Middlewares/messageAccessGuard.js"
 
 const router = express.Router()
 
-router.route("/send-message/:recieverId").post(verifyJWT, sendMessage)
+router.route("/send-message/:recieverId").post(verifyJWT, dmProtect, sendMessage)
 router.route("/get-messages/:userToChatId").get(verifyJWT, getMessages)
 
 export default router
