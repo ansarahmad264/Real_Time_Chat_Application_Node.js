18128a35756fd8cb73dc96a71e4c0b68cb812118
Ansar Ahmad <ahmadansar264@gmail.com>
2025-07-23 15:32:08 +0500

    minor changes for Hosting

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ dotenv.config({
 import connectDB from "./db/connection.js"
 import { app } from "./app.js"
 
+app.get("/", (req, res) => {
+    res.json({ success: true, data: {}, message: "Server is running ... .." })
+})
+
 connectDB()
 .then(() =>{
-    app.listen(process.env.PORT,() => {
+    app.listen(process.env.PORT, '0.0.0.0', () => {
         console.log(`Server has Started at Port: ${process.env.PORT}`);
     })
 })
