6d881d42c08a6c57a519eca361dccf50df40e19b
Ansar Ahmad <ahmadansar264@gmail.com>
2025-08-15 13:12:50 +0500

    update Project configurations

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,7 @@
 import admin from 'firebase-admin'
 import path from 'path'
 //download this file from firebase and add it to project root and save it here below
-import serviceAccount from "../../chatter-289f4-firebase-adminsdk-fbsvc-bd6ded5f47.json" assert { type: "json" };
+import serviceAccount from "../../livelink-6be26-firebase-adminsdk-fbsvc-189099b76d.json" assert { type: "json" };
  // your Firebase Admin SDK key
 
 admin.initializeApp({
